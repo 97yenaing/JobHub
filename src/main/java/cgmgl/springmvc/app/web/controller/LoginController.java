489b8b8087d24537651a70c8ef2181bd4fa947e7
YinYinSwe <yinyinswe1999@gamil.com>
2022-09-06 10:01:45 +0630

    get user info

--- src/main/java/cgmgl/springmvc/app/web/controller/LoginController.java
+++ src/main/java/cgmgl/springmvc/app/web/controller/LoginController.java
@@ -1,11 +1,20 @@
 package cgmgl.springmvc.app.web.controller;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import cgmgl.springmvc.app.bl.dto.UserDto;
+import cgmgl.springmvc.app.bl.service.UserService;
+import cgmgl.springmvc.app.persistence.entity.Authority;
+import cgmgl.springmvc.app.persistence.entity.User;
 
 /**
  * <h2> LoginController Class</h2>
@@ -18,6 +27,9 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class LoginController {
+    @Autowired
+    private UserService userService;
+    
     @RequestMapping(value = { "/" }, method = RequestMethod.GET)
     public String login() {
         return "login";
@@ -33,8 +45,10 @@ public class LoginController {
      * @return ModelAndView
      */
     @RequestMapping(value = "/home")
-    public ModelAndView homePage() {
+    public ModelAndView homePage(Principal authentication) {
+        System.out.println(authentication.getName());
         ModelAndView model = new ModelAndView();
+        User user = this.userService.doGetLoginInfo();
         model.setViewName("home");
         return model;
     }
@@ -79,7 +93,7 @@ public class LoginController {
      */
     @RequestMapping(value = "/applicant")
     public String applicantPage() {
-        return "applicant";
+        return "redirect:/userList";
     }
 
     /**
@@ -94,5 +108,11 @@ public class LoginController {
     @RequestMapping(value = "/admin")
     public String AdminPage() {
         return "admin";
+    }
+    @RequestMapping(value = "/error")
+    public ModelAndView errorPage() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("access-denied");
+        return model;
     }
 }
